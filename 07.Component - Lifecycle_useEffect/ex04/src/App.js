e98fcd884facb64d2ee6b37269e1578f69ecf1f8
DAY0522 <ekdud_____@naver.com>
2025-02-13 09:04:37 +0900

    clock #finish

--- 07.Component - Lifecycle_useEffect/ex04/src/App.js
+++ 07.Component - Lifecycle_useEffect/ex04/src/App.js
@@ -1,25 +1,37 @@
-import React, {useState, useEffect} from 'react';
-import './assets/scss/App.scss'
-import Clock from './Clock';
+import React, { useState, useEffect } from "react";
+import "./assets/scss/App.scss";
+import Clock from "./Clock";
 
 export default function App() {
-    const getCurrentTime = () => {
-        const now = new Date();
-        return {
-            hours: now.getHours(),
-            minutes: now.getMinutes(),
-            seconds: now.getSeconds()
-        }
-    }
+  const getCurrentTime = () => {
+    const now = new Date();
+    return {
+      hours: now.getHours(),
+      minutes: now.getMinutes(),
+      seconds: now.getSeconds(),
+    };
+  };
 
-    const [currentTime, setCurrentTime] = useState(getCurrentTime());
-    const [ticks, setTicks] = useState(0);
+  const [currentTime, setCurrentTime] = useState(getCurrentTime());
+  const [ticks, setTicks] = useState(0);
 
-    return (
-        <Clock
-            title={`ex04: Clock Component II: `}
-            hours={currentTime.hours}
-            minutes={currentTime.minutes}
-            seconds={currentTime.seconds} />
-    );
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentTime(getCurrentTime());
+      setTicks((ticks) => ticks + 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  return ticks % 10 === 0 ? null : (
+    <Clock
+      title={`ex04: Clock Component II: ${ticks}`}
+      hours={currentTime.hours}
+      minutes={currentTime.minutes}
+      seconds={currentTime.seconds}
+    />
+  );
 }
